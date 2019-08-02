9c34a53a29df7cba0ba9864f6c064796d0338b45
shiv.prakash <shiv.prakash@cgi.com>
2019-08-02 12:36:52 +0530

    find by name commit

---
+++ src/main/java/com/stackroute/controller/TrackController.java
@@ -0,0 +1,77 @@
+package com.stackroute.controller;
+
+import com.stackroute.domain.Track;
+import com.stackroute.service.TrackService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@Controller
+    @RequestMapping("/api/v1")
+    public class TrackController {
+        private TrackService trackService;
+
+
+        public TrackController(TrackService trackService) {
+            this.trackService = trackService;
+        }
+    //use postMapping to set tracks
+        @PostMapping("track")
+        public ResponseEntity<?> setTrack(@RequestBody Track track){
+            try {
+                Track savedTrack=trackService.saveTrack(track);
+                return new ResponseEntity<>(savedTrack, HttpStatus.OK);
+            }
+            catch (Exception ex){
+                return new ResponseEntity<>(ex.getMessage(), HttpStatus.CONFLICT);
+            }
+        }
+    //use getMapping to gettracksById
+        @GetMapping("track/{id}")
+        public ResponseEntity<?> getTrack(@PathVariable("id") int id) {
+            ResponseEntity responseEntity;
+            try {
+                Track track=trackService.getTrackById(id);
+                responseEntity=new ResponseEntity<>(track,HttpStatus.OK);
+            }
+           catch (Exception ex){
+                responseEntity=new ResponseEntity<>(ex.getMessage(),HttpStatus.CONFLICT);
+           }
+            return responseEntity;
+        }
+//use getMapping to get all tracks
+        @GetMapping("tracks")
+        public ResponseEntity<?> getAllTrack() {
+            return new ResponseEntity<List<Track>>(trackService.getAllTracks(), HttpStatus.OK);
+        }
+        //Use DeleteMapping to delete a particular track given by id
+        @DeleteMapping("track/{id}")
+        public ResponseEntity<?> deleteTrackById(@PathVariable int id) {
+            Optional<Track> trackRemoved = Optional.of(trackService.deleteTrackById(id));
+            return new ResponseEntity<>(trackRemoved, HttpStatus.OK);
+        }
+
+
+    //Use PatchMapping to update the element in database
+    @PatchMapping("/track/{id}")
+    public ResponseEntity<?> updateTrack(@RequestBody Track track, @PathVariable("id") int id) {
+        Track track1 = trackService.updateTrackById(id, track);
+        return new ResponseEntity < > (track1, HttpStatus.OK);
+    }
+///get track by name
+    @GetMapping("tracks/{name}")
+    public ResponseEntity<?> findByName(@PathVariable("name") String name) {
+        List<Track> foundTracks=trackService.findByName(name);
+        return new ResponseEntity<List<Track>>(foundTracks, HttpStatus.FOUND);
+    }
+
+
+    }
+
+
+
---
+++ src/main/java/com/stackroute/domain/Track.java
@@ -0,0 +1,57 @@
+package com.stackroute.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+@Entity
+public class Track {
+//variable declared for track id ,track name and comments
+    @Id
+    int id;
+    String name;
+    String comments;
+
+
+    public Track(int id, String name, String comments) {
+        this.id = id;
+        this.name = name;
+        this.comments = comments;
+    }
+
+    public Track() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getComments() {
+        return comments;
+    }
+
+    public void setComments(String comments) {
+        this.comments = comments;
+    }
+
+
+//to-string method
+    @Override
+    public String toString() {
+        return "Track{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", comments='" + comments + '\'' +
+                '}';
+    }
+}
